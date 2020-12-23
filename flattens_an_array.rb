ed4e4accc704a23422bf895b9ecd08f2280cde99
Farid Ali <faridalicode@gmail.com>
2020-12-23 20:05:24 +0530

    Define a recursive function that flattens an array

---
+++ flattens_an_array.rb
@@ -0,0 +1,14 @@
+#Define a recursive function that flattens an array. The method should convert [[1, 2], [3, 4]] to [1, 2, 3, 4] and [[1, [8, 9]], [3, 4]] to [1, 8, 9, 3, 4].
+
+def flatten(input_array, result = [])
+  input_array.each do |element|
+    if element.kind_of?(Array)
+      flatten(element, result)
+    else
+      result << element
+    end
+  end
+  result
+end
+
+p flatten([[1, 2], [3, 4]])
